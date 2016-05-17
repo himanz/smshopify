903872dfbcef26e0cd3342192651a818760965b4
Jonathan Chang <jonoschang7@gmail.com>
2016-05-17 00:44:27 -0400

    remove sms sessions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,8 +4,6 @@ class MessagesController < ApplicationController
   
 	skip_before_action :verify_authenticity_token
 
-	enable :sessions
-
 	def index
 		@client = Twilio::REST::Client.new ENV["twilio_sid"], ENV["twilio_auth_token"]
 
@@ -13,15 +11,12 @@ class MessagesController < ApplicationController
   end
 
 	def inbound
-		session["counter"] ||= 0
-    sms_count = session["counter"]
     sender = params[:From]
     
 		twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Hey there! I got a text from you. #{sender}. Message number #{sms_count + 1}"
+      r.Message "Hey there! I got a text from you. #{sender}"
     end
 
-    session["counter"] += 1
     render_twiml twiml
 	end
 
