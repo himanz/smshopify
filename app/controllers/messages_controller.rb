caaf194ba7a905f9a6994850cc2bbc8123fdccbe
Jonathan Chang <jonoschang7@gmail.com>
2016-05-17 00:41:15 -0400

    enable sessions for sms conversations

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,9 @@ class MessagesController < ApplicationController
   # after_filter :set_header
   
 	skip_before_action :verify_authenticity_token
+
+	enable :sessions
+
 	def index
 		@client = Twilio::REST::Client.new ENV["twilio_sid"], ENV["twilio_auth_token"]
 
@@ -10,11 +13,15 @@ class MessagesController < ApplicationController
   end
 
 	def inbound
+		session["counter"] ||= 0
+    sms_count = session["counter"]
     sender = params[:From]
-
+    
 		twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Hey there! I got a text from you. #{sender}"
+      r.Message "Hey there! I got a text from you. #{sender}. Message number #{sms_count + 1}"
     end
+
+    session["counter"] += 1
     render_twiml twiml
 	end
 
